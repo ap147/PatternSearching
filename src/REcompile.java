afd2a369b275035ab65dc6dec073d223201fb734
Amarjot Parmar <amarjot-parmar@hotmail.co.nz>
2017-04-15 17:55:42 +1200

    Deque class created (not completed yet)

--- src/REcompile.java
+++ src/REcompile.java
@@ -1,5 +1,96 @@
-/**
- * Created by Amarjot8 on 05-Apr-17.
- */
-public class REcompile {
+//Amarjot Parmar (1255668)
+
+
+public class REcompile
+{
+
+
+    public static void main(String[] args)
+    {
+
+    }
+
+
 }
+class deque
+{
+    node head;
+    node tail;
+
+    public deque()
+    {
+        head = null;
+        tail = null;
+    }
+    public void insertLeft()
+    {
+        if(head == null)
+        {
+            //head == node
+            //tail == node
+        }
+        else
+        {
+            //node.setnext = head
+            //head == node
+            //head.prev = null
+        }
+    }
+    public void insertRight()
+    {
+        if(tail == null)
+        {
+            //head = node
+            //tail = node
+        }
+        else
+        {
+            //tail.setnext = node
+            //node.setprev = tail
+            //tail = node
+
+        }
+    }
+
+    public void removeLeft()
+    {
+        head = head.getNext();
+        head.setPrev(null);
+    }
+    public void removeRight()
+    {
+        tail = tail.getPrev();
+        tail.setNext(null);
+    }
+
+
+    class node
+    {
+        node next;
+        node prev;
+
+        public node()
+        {
+
+        }
+        protected node getNext()
+        {
+            return next;
+        }
+        protected node getPrev()
+        {
+            return prev;
+        }
+        protected void setNext(node n)
+        {
+            next = n;
+        }
+        protected void setPrev(node p)
+        {
+            prev = p;
+        }
+    }
+
+}
+
+
