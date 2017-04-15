3cd17966ce2424f8319e9a193d6bbacea58afd73
Amarjot Parmar <amarjot-parmar@hotmail.co.nz>
2017-04-15 20:14:09 +1200

    i am a potato this class does not need a dq

--- src/REcompile.java
+++ src/REcompile.java
@@ -4,7 +4,11 @@
 public class REcompile
 {
 
-
+    /*
+    the state-number,
+    the input-symbol this state must match (or indication it is a branching state, or similar),
+     and two numbers indicating the two possible next states if a match is made.
+     */
     public static void main(String[] args)
     {
 
@@ -12,85 +16,6 @@ public class REcompile
 
 
 }
-class deque
-{
-    node head;
-    node tail;
 
-    public deque()
-    {
-        head = null;
-        tail = null;
-    }
-    public void insertLeft()
-    {
-        if(head == null)
-        {
-            //head == node
-            //tail == node
-        }
-        else
-        {
-            //node.setnext = head
-            //head == node
-            //head.prev = null
-        }
-    }
-    public void insertRight()
-    {
-        if(tail == null)
-        {
-            //head = node
-            //tail = node
-        }
-        else
-        {
-            //tail.setnext = node
-            //node.setprev = tail
-            //tail = node
-
-        }
-    }
-
-    public void removeLeft()
-    {
-        head = head.getNext();
-        head.setPrev(null);
-    }
-    public void removeRight()
-    {
-        tail = tail.getPrev();
-        tail.setNext(null);
-    }
-
-
-    class node
-    {
-        node next;
-        node prev;
-
-        public node()
-        {
-
-        }
-        protected node getNext()
-        {
-            return next;
-        }
-        protected node getPrev()
-        {
-            return prev;
-        }
-        protected void setNext(node n)
-        {
-            next = n;
-        }
-        protected void setPrev(node p)
-        {
-            prev = p;
-        }
-    }
-
-}
 
 
