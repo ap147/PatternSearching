09931d9dd8e38dab8d613377b59aaf72c0657f99
Amarjot Parmar <amarjot-parmar@hotmail.co.nz>
2017-04-17 23:35:34 +1200

    parser partly working

--- src/REcompile.java
+++ src/REcompile.java
@@ -1,13 +1,8 @@
 //Amarjot Parmar (1255668)
+//http://stackoverflow.com/questions/4047808/what-is-the-best-way-to-tell-if-a-character-is-a-letter-or-number-in-java-withou
 
 public class REcompile
 {
-
-    /*
-    the state-number,
-    the input-symbol this state must match (or indication it is a branching state, or similar),
-     and two numbers indicating the two possible next states if a match is made.
-     */
     static String [] p;
     static int j = 0;
     public static void main(String[] args)
@@ -15,13 +10,19 @@ public class REcompile
         System.err.println("Expersion : " + args[0]);
         p = args[0].split("");
         parse();
-       // System.out.println(isvocab(p[0]));
+        // System.out.println(isvocab(p[0]));
     }
-    public static void expression()
+    private static void parse()
+    {
+        System.out.println("par");
+        expression();
+        if(!p[j].equals('\0'))  System.out.println("error");
+    }
+
+    private static void expression()
     {
         System.err.println("exp " +p[j]+ " isvocab : " +   isvocab(p[j]));
         term();
-        System.err.println("exp " + p[j] + " isvocab : " + isvocab(p[j]));
         if(isvocab(p[j])||p[j].equals('(')) {
 
             expression();
@@ -33,8 +34,8 @@ public class REcompile
         System.err.println("term "+ p[j]+ " isvocab : " +   isvocab(p[j]));
 
         factor();
-        if(p[j].equals('*')) j++;
-        if(p[j].equals('+'))
+        if(p[j].equals("*")) j++;
+        if(p[j].equals("+"))
         {
             j++;
             term();
@@ -44,42 +45,39 @@ public class REcompile
     private static void factor()
     {
         System.err.println("factor : " +p[j]+ " isvocab : " +   isvocab(p[j]));
-        print();
+
         if(isvocab(p[j])) j++;
         else
-        if(p[j].equals('(')){
+        if(p[j].equals("(")){
             j++;    expression();
-            if(p[j].equals(')')) j++;
+            if(p[j].equals(")")) j++;
             else System.out.println("error");
         }
         else  System.out.println("error");
+
+        print();
     }
 
-    private static void parse()
-    {
-        System.out.println("par");
-        expression();
-        if(!p[j].equals('\0'))  System.out.println("error");
-    }
+
 
     private static boolean isvocab(String c)
     {
-
         char x = c.charAt(0);
-
         return Character.isAlphabetic(x);
-
     }
+
+
     private static void print()
     {
         System.out.println();
-        for(int x = 0; x < p.length; x++)
+        for(int x = j; x < p.length; x++)
         {
             System.out.print(p[x]);
         }
+        System.out.println();
     }
-
 }
+
 
 
 
