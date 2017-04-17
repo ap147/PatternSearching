0c59c7150a28b7fb3ec7af519342200c9d170a13
Amarjot Parmar <amarjot-parmar@hotmail.co.nz>
2017-04-18 04:05:21 +1200

    parser/complier fills 3 arrays with FMS but doesnt stop when expresion is finished and runs into error

--- src/REcompile.java
+++ src/REcompile.java
@@ -2,9 +2,13 @@
 //http://stackoverflow.com/questions/4047808/what-is-the-best-way-to-tell-if-a-character-is-a-letter-or-number-in-java-withou
 
 public class REcompile
-{
+{   static Character ch [] = new Character[20];
+    static Integer next1 [] = new Integer[20];
+    static Integer next2 [] = new Integer[20];
+    static Integer state=0;
     static String [] p;
     static int j = 0;
+
     public static void main(String[] args)
     {
         System.err.println("Expersion : " + args[0]);
@@ -15,47 +19,78 @@ public class REcompile
     private static void parse()
     {
         System.out.println("par");
-        expression();
-        if(!p[j].equals('\0'))  System.out.println("error");
+        int initial;
+
+        initial=expression();
+        if( p[j].equals("1") ) error(); // In C, zero is false, not zero is true
+        set_state(state,' ',0,0);
     }
 
-    private static void expression()
+    private static int expression()
     {
         System.err.println("exp " +p[j]+ " isvocab : " +   isvocab(p[j]));
-        term();
-        if(isvocab(p[j])||p[j].equals('(')) {
+        int r;
 
-            expression();
-        }
+        r=term();
+        if(isvocab(p[j])||p[j].equals("[")) expression();
+        return(r);
     }
 
-    private static void term()
+    private static int term()
     {
         System.err.println("term "+ p[j]+ " isvocab : " +   isvocab(p[j]));
 
-        factor();
-        if(p[j].equals("*")) j++;
-        if(p[j].equals("+"))
+        int r, t1,t2,f;
+        
+        f=state-1; r=t1=factor();
+        printArrays();
+        System.out.println(j);
+        if(p[j].equals("*"))
         {
-            j++;
-            term();
+            set_state(state,' ',state+1,t1);
+            j++; r=state; state++;
         }
+        if(p[j].equals("+")){
+            if(next1[f]==next2[f])
+                next2[f]=state;
+            next1[f]=state;
+            f=state-1;
+            j++;r=state;state++;
+            t2=term();
+            set_state(r,' ',t1,t2);
+            if(next1[f]==next2[f])
+                next2[f]=state;
+            next1[f]=state;
+        }
+        return(r);
     }
 
-    private static void factor()
+    private static int factor()
     {
         System.err.println("factor : " +p[j]+ " isvocab : " +   isvocab(p[j]));
 
-        if(isvocab(p[j])) j++;
-        else
-        if(p[j].equals("(")){
-            j++;    expression();
-            if(p[j].equals(")")) j++;
-            else System.out.println("error");
+        int r =0;
+
+        if(isvocab(p[j]))
+        {
+            char x = p[j].charAt(0);
+            set_state(state,x,state+1,state+1);
+            j++;r=state; state++;
         }
-        else  System.out.println("error");
+        else
+        if(p[j].equals("["))
+        {
+            j++; r=expression();
+            if(p[j].equals("]"))
+                j++;
+            else
+                error();
+        }
+        else
+            error();
 
         print();
+        return(r);
     }
 
 
@@ -66,7 +101,10 @@ public class REcompile
         return Character.isAlphabetic(x);
     }
 
-
+    private static void set_state(int s, char c, int n1, int n2)
+    {
+        ch[s]=c;next1[s]=n1;next2[s]=n2;
+    }
     private static void print()
     {
         System.out.println();
@@ -75,6 +113,20 @@ public class REcompile
             System.out.print(p[x]);
         }
         System.out.println();
+    }
+    private static void printArrays()
+    {
+        System.out.println();
+        System.out.println("s | ch 1 2");
+        for(int x =0; x< ch.length; x++)
+        {
+            System.out.println(x +" | " +ch[x] + " " + next1[x] + " " + next2[x]);
+        }
+    }
+    private static void error()
+    {
+        System.out.println("Error");
+        printArrays();
     }
 }
 
